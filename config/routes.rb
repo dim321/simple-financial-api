24d084a33e96dada41c6f5e3773bc4222ff012f3
dim321 <tunturidim@gmail.com>
2025-06-03 09:52:05 +0700

    set default format json for devise

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  devise_for :users, path: '', path_names: {
+  devise_for :users, defaults: { format: :json }, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
