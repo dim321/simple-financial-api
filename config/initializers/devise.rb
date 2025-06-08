8343265d8719bf39a4cc607ed3163c25666d7627
dim321 <tunturidim@gmail.com>
2025-06-08 20:19:36 +0700

    config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -314,12 +314,12 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
     jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
+      ['POST', %r{^/api/v1/auth/sign_in$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
+      ['DELETE', %r{^/api/v1/auth/sign_out$}]
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    jwt.expiration_time = 120.minutes.to_i
     jwt.request_formats = {
       user: [:json]
     }
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,31 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  devise_for :users, defaults: { format: :json }, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'sign_up'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users,
+             controllers: {
+               sessions: 'api/v1/auth/sessions',
+               registrations: 'api/v1/auth/registrations'
+             },
+             path: 'api/v1/auth',
+             defaults: { format: :json }
+
+  namespace :api do
+    namespace :v1 do
+      resources :accounts, only: %i[index show create] do
+        get 'balance', on: :collection
+        post 'deposit', on: :collection
+        post 'withdraw', on: :collection
+        post 'transfer', on: :collection
+        post 'hold', on: :collection
+        post 'unhold', on: :collection
+        post 'close', on: :collection
+
+        resources :transactions, only: [:index, :show] do
+          member do
+            post :reverse
+          end
+        end
+      end
+    end
+  end
 end
