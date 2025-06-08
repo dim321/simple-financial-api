87ba571ec2cc0ca4133ed08457c39b9edc1fecca
dim321 <tunturidim@gmail.com>
2025-06-08 20:26:22 +0700

    Readme and curl requests

--- README.md
+++ README.md
@@ -1,24 +1,12 @@
-# README
+Test task: Simple financial API
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Ruby 3.4.4
+Rails 8
 
-Things you may want to cover:
+# How to run the application
 
-* Ruby version
+1. Clone the repository
+2. Run `docker compose up --build`
+3. Check curl_examples.txt for request examples and try it in console.
+4. Enjoy
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_29_160225) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_07_030313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "account_number", null: false
+    t.decimal "balance", precision: 10, scale: 2, default: "0.0", null: false
+    t.string "currency", default: "USD", null: false
+    t.string "status", default: "active", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_number"], name: "index_accounts_on_account_number", unique: true
+    t.index ["user_id", "currency"], name: "index_accounts_on_user_id_and_currency", unique: true
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "source_account_id"
+    t.bigint "target_account_id"
+    t.decimal "amount", precision: 19, scale: 4, null: false
+    t.string "currency", null: false
+    t.string "transaction_type", null: false
+    t.string "status", default: "completed", null: false
+    t.text "description"
+    t.jsonb "metadata", default: {}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_transactions_on_created_at"
+    t.index ["source_account_id"], name: "index_transactions_on_source_account_id"
+    t.index ["status"], name: "index_transactions_on_status"
+    t.index ["target_account_id"], name: "index_transactions_on_target_account_id"
+    t.index ["transaction_type"], name: "index_transactions_on_transaction_type"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +59,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_29_160225) do
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "accounts", "users"
+  add_foreign_key "transactions", "accounts", column: "source_account_id"
+  add_foreign_key "transactions", "accounts", column: "target_account_id"
 end
