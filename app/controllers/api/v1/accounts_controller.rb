62d4bf360b8bf3a675a990674dc8be0e34788cc4
dim321 <tunturidim@gmail.com>
2025-06-29 17:46:01 +0700

    add gem rspec

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class AccountsController < ApplicationController
       include AccountErrors
-      
+
       before_action :set_account, except: :create
 
       def show
